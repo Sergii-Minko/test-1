4655231e00818d311a72b1006c21382f0ba876c4
Sergii Minko <minkosergii@gmail.com>
2024-04-22 16:43:45 +0200

    edit-5

--- src/js/reviews/render-functions.js
+++ src/js/reviews/render-functions.js
@@ -1,35 +1,17 @@
 export const createReviewsList = reviews => {
   return reviews
     .map(
-      reviewData => ` 
-     <li class=" reviews-item-container card swiper-slide" id="${
-       reviewData._id - 1
-     }">
-     
-       <div class="reviews-item details">
-           <img
+      reviewData => `
+      <li class="reviews-item-container card swiper-slide" id="${reviewData._id}">     
+            <img
               class="reviews-avatar"
               alt="${reviewData.author}"
               src="${reviewData.avatar_url}" 
-              loading="lazy"
-            />          
-
-          <h3 class="reviews-name">${reviewData.author}</h3>
-          <p class="reviews-text">
-            ${reviewData.review}
-          </p>
-        </div>
-        <div class="imgBx">
-          <img
-            class="reviews-img"
-            alt="${reviewData.author} grossest"
-            src="${reviewData.avatar_url}"
-            loading="lazy"
-          />
-        </div>
-     
-    </li>
-  `
+              loading="lazy"/>
+              <h3 class="reviews-name">${reviewData.author}</h3>
+              <p class="reviews-text"> ${reviewData.review}</p>
+      </li>
+          `
     )
     .join('');
 };
