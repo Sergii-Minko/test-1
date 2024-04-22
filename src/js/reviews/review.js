4f7f47ab042b0c1f9b75b6c0239947ab8afaabc1
Sergii Minko <minkosergii@gmail.com>
2024-04-22 15:50:11 +0200

    edit-3

--- src/js/reviews/review.js
+++ src/js/reviews/review.js
@@ -79,21 +79,13 @@ fetchReviews(urlapi).then(reviews => {
   });
 });
 
-const cards = document.querySelectorAll('.card');
-
-cards.forEach(card => {
-  card.addEventListener('touchstart', () => {
-    card.focus();
-  });
-});
-
-reviewlist.addEventListener('click', function (event) {
-  event.preventDefault();
-  let targetElement = event.target;
-  let children = this.children;
-  Array.from(children).forEach(function (child) {
-    if (child.contains(targetElement)) {
-      child.classList.toggle('click');
-    }
-  });
-});
+// reviewlist.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   let targetElement = event.target;
+//   let children = this.children;
+//   Array.from(children).forEach(function (child) {
+//     if (child.contains(targetElement)) {
+//       child.classList.toggle('click');
+//     }
+//   });
+// });
